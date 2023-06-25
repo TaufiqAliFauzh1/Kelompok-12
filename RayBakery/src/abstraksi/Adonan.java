0dd9b4dafdcace65306e2a703e9a1fad85c415e7
Taufiq Ali <taufiq.ali.fauzhi@gmail.com>
2023-06-25 18:32:53 +0700

    Update Adonan 2.java

--- RayBakery/src/abstraksi/Adonan.java
+++ RayBakery/src/abstraksi/Adonan.java
@@ -23,4 +23,18 @@ public abstract class Adonan {
 
   public int totalPesanan;
 
+  public abstract void hitungBeratBahan();
+  public double hitungHargaBahan(){
+    double hargaTepung=(40000/1000)*this.beratTepungPSN;
+    double hargaGula=(25000/1000)*this.beratGulaPSN;
+    double hargaButter=(23000/500)*this.beratButterPSN;
+    double hargaRagi=(5000/11)*this.beratRagiPSN;
+    double hargaSusuBubuk=(39000/1000)*this.beratSusuBubukPSN;
+    double hargaSusuCair=(24000/1000)*this.beratSusuCairPSN;
+    double hargaTelur=(23000/1000)*this.beratTelurPSN;
+    double hargaEsBatu=(2000/1000)*this.beratEsBatuPSN;
+       
+    double hargaBahan=hargaTepung+hargaGula+hargaButter+hargaRagi+hargaSusuBubuk+hargaSusuCair+hargaTelur+hargaEsBatu;
+    return hargaBahan;
+    }
 }
