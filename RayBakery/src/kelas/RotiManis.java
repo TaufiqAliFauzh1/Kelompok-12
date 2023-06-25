94f105411aa1e99ff85df3a521955523403732e4
Taufiq Ali <taufiq.ali.fauzhi@gmail.com>
2023-06-25 20:25:05 +0700

    Override hitung berat bahan.java

--- RayBakery/src/kelas/RotiManis.java
+++ RayBakery/src/kelas/RotiManis.java
@@ -12,5 +12,17 @@ package kelas;
  */
 
 public class RotiManis extends abstraksi.Adonan{
-
+  
+  @Override
+  public void hitungBeratBahan(){
+    double jumlahRotiPerAdonan= 2350/50;
+    this.beratTepungPSN=(1000/jumlahRotiPerAdonan)*totalPesanan;
+    this.beratGulaPSN=(150/jumlahRotiPerAdonan)*totalPesanan; 
+    this.beratButterPSN=(150/jumlahRotiPerAdonan)*totalPesanan;
+    this.beratRagiPSN=(20/jumlahRotiPerAdonan)*totalPesanan;
+    this.beratSusuBubukPSN=(300/jumlahRotiPerAdonan)*totalPesanan;
+    this.beratSusuCairPSN=(250/jumlahRotiPerAdonan)*totalPesanan;
+    this.beratTelurPSN=(80/jumlahRotiPerAdonan)*totalPesanan;
+    this.beratEsBatuPSN=(400/jumlahRotiPerAdonan)*totalPesanan;
+    }
 }
