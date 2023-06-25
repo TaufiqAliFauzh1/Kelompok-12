c08a327f4890054a43d012dc44f31baf8238ec8b
dianfitriutami <111755680+dianfitriutami@users.noreply.github.com>
2023-06-25 21:56:48 +0700

    Update RotiManisVarian1.java
    variann 1 done

--- RayBakery/src/varian/RotiManisVarian1.java
+++ RayBakery/src/varian/RotiManisVarian1.java
@@ -14,5 +14,58 @@ import interfaces.Topping;
  * @author Dian
  */
 public class RotiManisVarian1 extends kelas.RotiManis implements Filling, Topping{
+  int beratKeju=5;
+  double beratKejuPSN;
 
+  int beratCoklatFilling=5;
+  double beratCoklatFillingPSN;
+
+  int beratCoklatTopping=5;
+  double beratCoklatToppingPSN;
+
+  @Override
+  public void hitungBeratFilling() {
+    this.beratKejuPSN = this.beratKeju * totalPesanan;
+    this.beratCoklatFillingPSN = this.beratCoklatFillingPSN * totalPesanan;
+
+    System.out.println("Berat Filling Keju: " + beratKejuPSN + " gr");
+    System.out.println("Berat Filling Coklat: " + beratCoklatFillingPSN + " gr");
+  }
+
+  @Override
+  public double hitungHargaFilling(){
+    double hargaCoklatFilling = (31000/250) * beratCoklatFillingPSN;
+    double hargaKeju = (29000/500) * beratKejuPSN;
+    double hargaFilling = hargaCoklatFilling + hargaKeju;
+    return hargaFilling;
+  }
+
+  @Override
+  public void hitungBeratTopping(){
+    this.beratCoklatToppingPSN = this.beratCoklatTopping * totalPesanan;
+    
+    System.out.println("Berat Topping Coklat: " + beratCoklatToppingPSN + " gr");
+  }
+
+  @Override
+  public double hitungHargaTopping(){
+    double hargaCoklatTopping = (29000/500) * beratCoklatToppingPSN;
+    return hargaCoklatTopping;
+  }
+
+  public void tampilkan(){
+    System.out.println("Roti Manis Varian 1");
+    System.out.println("Jumlah: " + totalPesanan + " pcs \n");
+    hitungBeratBahan();
+    hitungBeratFilling();
+    hitungBeratTopping();
+    System.out.println("-------------------");
+    System.out.println("Harga Bahan: Rp." + hitungHargaBahan());
+    System.out.println("Harga Filling: Rp." + hitungHargaFilling());
+    System.out.println("Harga Topping: Rp." + hitungHargaTopping());
+    
+    double hargaJual= hitungHargaBahan() + hitungHargaFilling() + hitungHargaTopping() + ((hitungHargaBahan() + hitungHargaFilling() + hitungHargaTopping()) * (40/100));
+    System.out.println("Harga Jual Per pcs: Rp." + hargaJual/totalPesanan);
+    System.out.println("\n \n");
+  }
 }
