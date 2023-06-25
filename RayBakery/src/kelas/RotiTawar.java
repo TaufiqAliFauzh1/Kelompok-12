4b16b58e685647a373871d6e60673c32393ed736
Taufiq Ali <taufiq.ali.fauzhi@gmail.com>
2023-06-25 23:35:59 +0700

    Done RotiTawar.java

--- RayBakery/src/kelas/RotiTawar.java
+++ RayBakery/src/kelas/RotiTawar.java
@@ -12,5 +12,26 @@ package kelas;
  */
 
 public class RotiTawar extends abstraksi.Adonan{
-
+  
+    @Override
+    public void hitungBeratBahan() {
+      double jumlahRotiPerAdonan= 2065/400;
+      this.beratTepungPSN=(1000/jumlahRotiPerAdonan)*totalPesanan;
+      this.beratGulaPSN=(125/jumlahRotiPerAdonan)*totalPesanan; 
+      this.beratButterPSN=(100/jumlahRotiPerAdonan)*totalPesanan;
+      this.beratRagiPSN=(20/jumlahRotiPerAdonan)*totalPesanan;
+      this.beratSusuBubukPSN=(250/jumlahRotiPerAdonan)*totalPesanan;
+      this.beratSusuCairPSN=(200/jumlahRotiPerAdonan)*totalPesanan;
+      this.beratTelurPSN=(70/jumlahRotiPerAdonan)*totalPesanan;
+      this.beratEsBatuPSN=(300/jumlahRotiPerAdonan)*totalPesanan;
+      
+      System.out.println("Berat Tepung: "+beratTepungPSN+" gr");
+      System.out.println("Berat Gula: "+beratGulaPSN+" gr");
+      System.out.println("Berat Butter: "+beratButterPSN+" gr");
+      System.out.println("Berat Ragi: "+beratRagiPSN+" gr");
+      System.out.println("Berat Susu Bubuk: "+beratSusuBubukPSN+" gr");
+      System.out.println("Berat Susu Cair: "+beratSusuCairPSN+" gr");
+      System.out.println("Berat Telur: "+beratTelurPSN+" gr");
+      System.out.println("Berat Es Batu: "+beratEsBatuPSN+" gr");     
+    }  
 }
