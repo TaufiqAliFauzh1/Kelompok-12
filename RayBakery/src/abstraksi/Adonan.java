e3f08f3ddbbe85630d19aa1ea6aaadedf5195519
Taufiq Ali <taufiq.ali.fauzhi@gmail.com>
2023-06-25 18:58:53 +0700

    Update Adonan 3.java

--- RayBakery/src/abstraksi/Adonan.java
+++ RayBakery/src/abstraksi/Adonan.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-//  Taufiq membuat kelas abstrak adonan dan jumlah pesanan, serta menghitung berat bahan
+//  Taufiq membuat kelas abstrak adonan dan jumlah pesanan, serta menghitung harga bahan
 package abstraksi;
 
 /**
