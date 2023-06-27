b5391e0770a8625f477126654eec871dc8107541
Raditya88 <112364728+Raditya88@users.noreply.github.com>
2023-06-27 14:00:54 +0700

    Update RotiManisVarian3ke2.java
    pusing

--- RayBakery/src/varian/RotiManisVarian3.java
+++ RayBakery/src/varian/RotiManisVarian3.java
@@ -20,12 +20,33 @@ public class RotiManisVarian3 extends kelas.RotiManis implements Filling{
     double beratSosisPSN;
 
     @override
-    public void hitungBeratFilling() {
+    public void hitungBeratFilling(){
         this.beratKejuPSN = this.beratKeju*totalPesanan;
         this.beratSosisPSN = this.beratSosis*totalPesanan;
 
         System.out.println("Berat Filling Keju: "+ beratKejuPSN +" gr")
         System.out.println("Berat Filling Sosis: "+ beratSosisPSN +" gr")
+    }
+    
+    @override
+    public double hitungHargaFilling(){
+        double hargaSosis = (31000/250)*beratSosisPSN;
+        double hargaKeju = (29000/500)*beratKejuPSN;
+        double hargaFilling = hargaSosis + hargaKeju;
+        return hargaFilling;
+    }
+    
+    public void tampilkan(){
+        System.out.println("Roti Manis Varian 3");
+        System.out.println("Jumlah: "+totalPesanan+" pcs \n");
+        hitungBeratBahan();
+        hitungBeratFilling();
+        System.out.println("-------------------");
+        System.out.println("Harga Bahan: Rp"+hitungHargaBahan());
+        System.out.println("Harga Filling: Rp"+hitungHargaFilling());
+        double hargaJual= hitungHargaBahan()+hitungHargaFilling()+((hitungHargaBahan()+hitungHargaFilling())*(50/100));
+        System.out.println("Harga Jual Per pcs: Rp"+hargaJual/totalPesanan);
+        System.out.println("\n \n");
+    }
 }
-
     
