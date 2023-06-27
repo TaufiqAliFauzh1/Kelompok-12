5357f388148631505356bb0af1eeee1a7978c664
dianfitriutami <111755680+dianfitriutami@users.noreply.github.com>
2023-06-27 15:20:38 +0700

    Update RotiTawarVarian1.java
    fiks

--- RayBakery/src/varian/RotiTawarVarian1.java
+++ RayBakery/src/varian/RotiTawarVarian1.java
@@ -13,31 +13,32 @@ package varian;
 public class RotiTawarVarian1 extends kelas.RotiTawar implements Filling{
     int beratCoklat=80;
     double beratCoklatPSN;
-
+    
     @Override
-    public void hitungBeratFilling(){
-        this.beratCoklatPSN = this.beratCoklat * totalPesanan;
-        
-        System.out.println("Berat Coklat: " + beratCoklatPSN + " gr");
+    public void hitungBeratFilling() {
+        this.beratCoklatPSN=this.beratCoklat*totalPesanan;
+        System.out.println("Berat Coklat: "+beratCoklatPSN+" gr");
     }
 
     @Override
-    public double hitungHargaFilling(){
-        double hargaCoklat = (29000/500) * beratCoklatPSN;
+    public double hitungHargaFilling() {
+        double hargaCoklat=(29000/500)*beratCoklatPSN;    
         return hargaCoklat;
     }
     
     public void tampilkan(){
         System.out.println("Roti Tawar Varian 1");
-        System.out.println("Jumlah: " + totalPesanan + "pcs \n");
+        System.out.println("Jumlah: "+totalPesanan+" pcs \n");
         hitungBeratBahan();
         hitungBeratFilling();
         System.out.println("-------------------");
-        System.out.println("Harga Bahan: Rp." + hitungHargaBahan());
-        System.out.println("Harga Filling: Rp." + hitungHargaFilling());
-
-        double hargaJual = hitungHargaBahan() + hitungHargaFilling() + ((hitungHargaBahan() + hitungHargaFilling()) * 40/100);
-        System.out.println("Harga Jual Per pcs: Rp." + hargaJual/totalPesanan);
+        System.out.println("Harga Bahan: Rp"+hitungHargaBahan());
+        System.out.println("Harga Filling: Rp"+hitungHargaFilling());
+        
+        double hargaJual= hitungHargaBahan()+hitungHargaFilling()+((hitungHargaBahan()+hitungHargaFilling())*50/100);
+        System.out.println("Harga Jual Per pcs: Rp"+hargaJual/totalPesanan);
         System.out.println("\n \n");
     }
+    
 }
+
